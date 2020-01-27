0266a7a3bc00ad6049449d9175ed6c959162842b
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-27 14:17:40 +0200

    nesusirisa failai

--- SavDarbas2+unitTest2/PalmesVaisiai/Program.cs
+++ SavDarbas2+unitTest2/PalmesVaisiai/Program.cs
@@ -6,6 +6,36 @@ using System.Threading.Tasks;
 
 namespace PalmesVaisiai
 {
+    struct Palme
+    {
+        public int Amzius;
+        public int VaisiuKiekis;
+
+        public int amzius
+        {
+            get { return Amzius; }
+            set { }
+        }
+
+        public int vaisiusKiekis
+        {
+            get { return VaisiuKiekis; }
+            set { }
+        }
+
+        public void Prideti1MenAmziaus()
+        {
+
+            if ((amzius + 1) > 5 && (amzius + 1) < 12)
+            {
+                VaisiuKiekis = (amzius + 1 - 5) * 3;
+            }
+            else
+            {
+                VaisiuKiekis = 0;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
--- SavDarbas2+unitTest2/SavDarbas2+unitTest2/Program.cs
+++ SavDarbas2+unitTest2/SavDarbas2+unitTest2/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PalmesVaisiai;
 
 namespace SavDarbas2_unitTest2
 {
+    
     class Program
     {
         static void Main(string[] args)
