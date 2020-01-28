c967daf1282333b98cc591ecdedd94527db73273
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-28 14:51:26 +0200

    kazkas ne taip

---
+++ SavDarbas4+unitTest/SavDarbas4+unitTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavDarbas4_unitTest
+{
+     public struct Miskas
+    {
+        public List<int> medziai;
+
+        public void PasodintiMedi()
+        {
+
+            int medis = 1;
+            medziai.Add(medis);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Miskas miskas;
+            miskas.medziai = new List<int>();
+
+            for (int i = 0; i <= 16; i++);
+            {
+                miskas.PasodintiMedi();
+            }
+            Console.WriteLine("{}", miskas.medziai);
+            Console.ReadLine();
+
+
+        }
+    }
+}
